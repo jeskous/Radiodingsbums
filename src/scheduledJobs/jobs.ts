b2295fc4d3b9abf3eea038d6c0203bd1b0d8165d
Jesko Usinger <oksej.10@googlemail.com>
2021-11-06 14:09:45 +0100

    change interval

--- src/scheduledJobs/jobs.ts
+++ src/scheduledJobs/jobs.ts
@@ -9,7 +9,7 @@ let lsNDR2: string;
 //jobs
 export const Jobs = {
   ndr2: async () => {
-    schedule.scheduleJob("*/2 * * * *", async () => {
+    schedule.scheduleJob("*/30 */1 * * * *", async () => {
       const song = await getNdr2Song();
       await createNewSong(lsNDR2, song);
       lsNDR2 = song.title;
@@ -17,7 +17,7 @@ export const Jobs = {
   },
 
   njoy: () => {
-    schedule.scheduleJob("*/2 * * * *", async () => {
+    schedule.scheduleJob("*/30 */1 * * * *", async () => {
       const song = await getNjoySong();
       await createNewSong(lsNJOY, song);
       lsNJOY = song.title;
