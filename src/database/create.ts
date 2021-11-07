14f78334b06aeeeb00db4963db19626deeb97b1f
Jesko Usinger <oksej.10@googlemail.com>
2021-11-07 14:02:27 +0100

    add closing browser

--- src/database/create.ts
+++ src/database/create.ts
@@ -3,18 +3,13 @@ import CurrentSong from "../interfaces/CurrentSong";
 const prisma = new PrismaClient();
 
 export async function createNewSong(lastSong: string, song: CurrentSong) {
-  try {
-    if (lastSong !== song.title) {
-      await prisma.song.create({
-        data: {
-          interpret: song.interpret,
-          title: song.title,
-          channel: song.channel,
-        },
-      });
-    }
-  } catch (err) {
-    console.log("createNewSong - Error");
-    console.log(err);
+  if (lastSong !== song.title) {
+    await prisma.song.create({
+      data: {
+        interpret: song.interpret,
+        title: song.title,
+        channel: song.channel,
+      },
+    });
   }
 }
--- src/scraper/ndr2/Ndr2Scraper.ts
+++ src/scraper/ndr2/Ndr2Scraper.ts
@@ -4,29 +4,26 @@ import CurrentSong from "../../interfaces/CurrentSong";
 const url: string = "https://www.ndr.de/ndr2/index.html";
 
 export async function getNdr2Song() {
-  try {
-    console.log("launcing browser...");
-    const browser = await puppeteer.launch({
-      args: ["--no-sandbox", "--disable-setuid-sandbox"],
-    });
-    console.log("opening new Page...");
-    const page = await browser.newPage();
-    console.log("going to url...");
-    await page.goto(url);
-    console.log("getting element...");
-    const title = await page.$eval("#titleNow", (el) => el.innerHTML);
-    const interpret = await page.$eval("#interpretNow", (el) => el.innerHTML);
-    console.log("creating object...");
-    const song: CurrentSong = {
-      title: title,
-      interpret: interpret,
-      channel: "NDR2",
-    };
-    console.log("returning new Song!");
-    console.log(song);
-    return song;
-  } catch (err) {
-    console.log("getNdr2Song - ERROR");
-    console.log(err);
-  }
+  console.log("launcing browser...");
+  const browser = await puppeteer.launch({
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
+  console.log("opening new Page...");
+  const page = await browser.newPage();
+  console.log("going to url...");
+  await page.goto(url);
+  console.log("getting element...");
+  const title = await page.$eval("#titleNow", (el) => el.innerHTML);
+  const interpret = await page.$eval("#interpretNow", (el) => el.innerHTML);
+  console.log("creating object...");
+  const song: CurrentSong = {
+    title: title,
+    interpret: interpret,
+    channel: "NDR2",
+  };
+  console.log("returning new Song!");
+  console.log(song);
+  console.log("closing browser...");
+  browser.close();
+  return song;
 }
